6f31672880d29c13d863d99cc44a101e552d7b73
Demez <demizexp@gmail.com>
2019-06-29 23:07:16 -0400

    HUGE changes, very awful commit - Recreated ParseLine() so you don't have forced formatting - added support for multiple videos to be merged into one - updated timestamps.txt example

--- ffmpeg_video_splitter.py
+++ ffmpeg_video_splitter.py
@@ -43,7 +43,9 @@ def FindArgument( search, return_value = False ):
 
 def RemoveComments( line ):
     line_new = line.split( "//", 1 )[0] # comment type 1
-    line_new = RemoveMultiLineComments( line_new )
+    if line_new != None:
+        line_new = RemoveMultiLineComments( line_new )
+
     return line_new
 
 def RemoveMultiLineComments( line ):
@@ -99,14 +101,35 @@ def FindMultiLineCommentStart( line ):
         return line_new
     return line
 
-# only removes tabs, new lines, and makes sure the output after that isn't empty
-def RemoveExtraCharacters( line ):
+# removes tabs, new lines, makes sure the output after that isn't empty
+# also 
+def FixLineCharacters( line ):
 
     new_line = ''.join( line.split( "\t" ) )
     new_line = ''.join( new_line.split( "\n" ) )
+    
+    in_quote = False
+    new_new_line = "" # nice var name, lmao
 
-    if new_line != '':
-        return new_line
+    # remove spaces in between quotes by checking each character and checking if we are in quote or not
+    for char in new_line:
+        if char == "\"":
+            in_quote = not in_quote
+
+        if char == " " and not in_quote:
+            continue
+        else:
+            new_new_line = new_new_line + char
+
+    # now fix any incorrect path seperators
+    # TODO: set up for posix
+    if os.name == "nt":
+        if '/' in new_new_line:
+            # split by "/" and then join it right after with the correct character
+            new_new_line = '\\'.join( new_new_line.split( '/' ) ) 
+
+    if new_new_line != '':
+        return new_new_line
     return None
 
 # this is fucking disgusting probably
@@ -122,82 +145,136 @@ def CheckIfDictExists( dictionary, key, value_type ):
         elif value_type == "str":
             dictionary[ key ] = ""
 
-# am i disabled
+# TODO: add support for full filepaths, then you can delete the above
+# also clean up the global vars if you can
 def ParseLine( line ):
 
-    global in_file_settings
+    global sub_block_num
+    global current_block
+    global filename
+    global video_blocks
     global file_index
-    global file_name
-    global input_file_folder
+    global out_folder
 
-    if line == "{":
-        in_file_settings = True
+    if sub_block_num == 0:
         file_index = 0
-        return
 
-    if line == "}": 
-        in_file_settings = False
-        file_index = 0
-        file_name = ""
-        return
+    if '"' in line:
 
-    if in_file_settings == False:
-
-        # could be an empty line, so the filename has to be in quotes
-        if '"' in line:
-            # new video file
-            quote_split = line.split( '"' ) # probably a bad idea, change this to split it by this
-            file_name = quote_split[1]
-
-            if os.sep in file_name:
-                input_file_folder = file_name.split( os.sep )[0] + os.sep
-                file_name = file_name.split( os.sep )[1]
-
-            elif '/' in file_name:
-                input_file_folder = file_name.split( '/' )[0] + os.sep
-                #root_dir = root_dir + file_name.split( '/' )[0] + os.sep
-                file_name = file_name.split( '/' )[1]
-
-            CheckIfDictExists( ffmpeg_split_dict, file_name, "dict" )
-            
-            #print( file_name )
-
-    else:
-        quote_split = line.split( '"' ) # probably a bad idea, change this to split it by this
+        # new video file
+        # split the line and check string by string
+        line_split = line.split( '"' )
         
-        start = quote_split[1]
-        end = quote_split[3]
+        # might be a key?
+        if line.startswith( "output_folder" ):
+            out_folder = ""
 
-        sub_clip = "sub_clip_" + str( file_index )
-        CheckIfDictExists( ffmpeg_split_dict[ file_name ], sub_clip, "list" )
+        str_index = 0
+        while str_index < len( line_split ):
 
-        # add the start and end times
-        ffmpeg_split_dict[ file_name ][ sub_clip ].append( start )
-        ffmpeg_split_dict[ file_name ][ sub_clip ].append( end )
+            string = line_split[ str_index ]
 
-        # increment this
-        file_index += 1
+            if string == "output_folder":
+                out_folder = line_split[ str_index + 2 ]
+                return # nothing else should be in this line
 
-#def ParseLineV2( line ):
+        #for string in quote_split:
+            if "{" in string:
+                for char in string:
+                    if char == "{":
+                        sub_block_num += 1
+                str_index += 1
+                continue # skip to the next string
+            elif "}" in string:
+                for char in string:
+                    if char == "}":
+                        sub_block_num -= 1
+                str_index += 1
+                continue
+            elif string == '' or string == ',': # comma is here just to make the txt file neater, completely optional lmao
+                str_index += 1
+                continue
+
+            if sub_block_num == 0:
+                filename = string
+                CheckIfDictExists( video_blocks, filename, "dict" )
+
+            elif sub_block_num == 1:
+                
+                current_block = string
+
+                # TODO: replace the ":" check with quote split, if there is one value, it's a block
+                # otherwise, it's a key and a value
+                # is it actually a timestamp?
+                # check for os.sep because it could be an absolute path
+                if ":" in string and not os.sep in string:
+                    CheckIfDictExists( video_blocks[ filename ], filename, "dict" )
+
+                    sub_clip = "sub_clip_" + str( file_index )
+                    CheckIfDictExists( video_blocks[ filename ][ filename ], sub_clip, "list" )
+
+                    # add the timestamp
+                    video_blocks[ filename ][ filename ][ sub_clip ].append( line_split[ str_index ] )
+                    video_blocks[ filename ][ filename ][ sub_clip ].append( line_split[ str_index + 2 ] )
+                    str_index += 2 # skip past the end timestamp
+                    file_index += 1
+                else:
+                    CheckIfDictExists( video_blocks[ filename ], string, "dict" )
+
+            elif sub_block_num == 2:
+
+                sub_clip = "sub_clip_" + str( file_index )
+                CheckIfDictExists( video_blocks[ filename ][ current_block ], sub_clip, "list" )
+
+                # add the timestamp
+                video_blocks[ filename ][ current_block ][ sub_clip ].append( line_split[ str_index ] )
+                video_blocks[ filename ][ current_block ][ sub_clip ].append( line_split[ str_index + 2 ] )
+                str_index += 2 # skip past the end timestamp
+                file_index += 1
+                #continue
+
+            str_index += 1
+
+                
+    elif "{" in line:
+        for char in line:
+            if char == "{":
+                sub_block_num += 1
+
+    elif "}" in line:
+        for char in line:
+            if char == "}":
+                sub_block_num -= 1
 
 # --------------------------------------------------------------------------------------------------------
 
 config_file = FindArgument( "-config", True )
-ffmpeg = FindArgument( "-ffmpeg", True )
+
+#if os.name == "nt":
+ffmpeg_bin = FindArgument( "-ffmpeg_bin", True )
+
+if ffmpeg_bin != None:
+    if not ffmpeg_bin.endswith( os.sep ):
+        if ffmpeg_bin.endswith( '"' ):
+            ffmpeg_bin = ffmpeg_bin.rsplit( '"', 1 )[0] + os.sep
+        else:
+            ffmpeg_bin = ffmpeg_bin + os.sep
+
+#if ffmpeg == None:
+    # on linux you can just put this in the command line, so if you don't specify -ffmpeg, it will just use this (bad idea?)
+    #ffmpeg = "ffmpeg"
+
 final_encode = FindArgument( "/final" )
-slightly_smaller_filesize = FindArgument( "/small_size" )
 
 root_dir = config_file.rsplit( os.sep, 1 )[0] + os.sep
 
-global ffmpeg_split_dict
-ffmpeg_split_dict = {}
-
 # wow
 file_index = 0
-input_file_folder = ""
-file_name = ""
-in_file_settings = False
 in_multiline_comment = False
+sub_block_num = 0
+current_block = ""
+filename = ""
+video_blocks = {}
 
 # main
 with open( config_file, mode = "r", encoding = "utf-8" ) as config:
@@ -210,12 +287,10 @@ with open( config_file, mode = "r", encoding = "utf-8" ) as config:
         if line_new == None:
             continue
 
-        line_new = RemoveExtraCharacters( line_new )
+        line_new = FixLineCharacters( line_new )
 
         if line_new == None:
             continue
-
-        #print( line_new )
 
         ParseLine( line_new )
          
@@ -244,7 +319,7 @@ def RemoveDirectory( directory ):
     if os.path.isdir( directory ):
         os.rmdir( directory )
 
-def RunFFMpegConCat( sub_video_list, output_video ):
+def RunFFMpegConCat( sub_video_list, out_video ):
 
     with open ( "temp.txt", "w", encoding = "utf-8" ) as temp_file:
         for sub_video in sub_video_list:
@@ -257,7 +332,7 @@ def RunFFMpegConCat( sub_video_list, output_video ):
     ffmpeg_command = []
     input_list = []
 
-    ffmpeg_command.append( ffmpeg )
+    ffmpeg_command.append( ffmpeg_bin + "ffmpeg" )
     ffmpeg_command.append( "-y" )
     ffmpeg_command.append( "-safe 0" )
 
@@ -268,31 +343,35 @@ def RunFFMpegConCat( sub_video_list, output_video ):
     ffmpeg_command.append( "-map 0" )
 
     # output file
-    ffmpeg_command.append( '"' + output_video + '"' )
+    ffmpeg_command.append( '"' + out_video + '"' )
 
-    subprocess.run( ' '.join( ffmpeg_command ) )
+    subprocess.run( ' '.join( ffmpeg_command ), shell = True )
     
     os.remove( "temp.txt" )
 
-def RunFFMpegSubVideo( video, obs_replay_buffer, output_name, start_time, length_time ):
+def RunFFMpegSubVideo( in_video, out_video, start_time, length_time ):
 
     ffmpeg_command = []
 
-    ffmpeg_command.append( ffmpeg )
+    ffmpeg_command.append( ffmpeg_bin + "ffmpeg" )
     ffmpeg_command.append( "-y" )
     ffmpeg_command.append( "-ss" )
     ffmpeg_command.append( start_time )
-    ffmpeg_command.append( '-i "' + video + '"' )
+    ffmpeg_command.append( '-i "' + in_video + '"' )
 
-    # TODO: add support for using ffprobe to get info of the audio channels of the video
-    if obs_replay_buffer:
+    # get audio track count
+    audio_tracks = GetAudioTrackCount( in_video )
+
+    # this is really just a hardcoded hack since this is what i use it for lmao
+    if audio_tracks == 5:
         ffmpeg_command.append( "-filter_complex \"[0:a:1][0:a:2]amerge[out]\"" )
         ffmpeg_command.append( "-map \"[out]\"")
-    else:
+    elif audio_tracks == 2:
         # need to check if the audio clip only has one track or not with ffprobe
         ffmpeg_command.append( "-filter_complex \"[0:a:0][0:a:1]amerge[out]\"" )
         ffmpeg_command.append( "-map \"[out]\"")
-        #ffmpeg_command.append( "-map 0:a" )
+    else:
+        ffmpeg_command.append( "-map 0:a" )
         
     ffmpeg_command.append( "-map 0:v" )
 
@@ -311,55 +390,107 @@ def RunFFMpegSubVideo( video, obs_replay_buffer, output_name, start_time, length
     ffmpeg_command.append( length_time )
 
     # output file
-    ffmpeg_command.append( '"' + output_name + '"' )
+    ffmpeg_command.append( '"' + out_video + '"' )
 
-    subprocess.run( ' '.join( ffmpeg_command ) )
+    subprocess.run( ' '.join( ffmpeg_command ), shell = True )
 
-for video in ffmpeg_split_dict:
-    # may add an exclude list here later
+def GetAudioTrackCount( video ):
+
+    ffprobe_command = []
+
+    ffprobe_command.append( ffmpeg_bin + "ffprobe" )
+    ffprobe_command.append( "-loglevel error" )
+    ffprobe_command.append( "-show_entries stream=codec_type" )
+    ffprobe_command.append( "-of csv=p=0" )
+    ffprobe_command.append( '"' + video + '"' )
+
+    output = subprocess.check_output( ' '.join( ffprobe_command ), shell = True )
+
+    # now clean up that output and shove it into a list
+    stream_list = str( output ).split( "\\n" )
+
+    audio_tracks = 0
+    for stream in stream_list:
+        if "audio" in stream:
+            audio_tracks += 1
+
+    return audio_tracks
+
+temp_path = root_dir + "TEMP_SPLIT" + os.sep
+out_dir = root_dir + "concat" + os.sep
+
+CreateDirectory( temp_path )
+
+# some shitty thing to print what we just parsed
+for out_video in video_blocks:
+    print( out_video )
+
+    for in_video in video_blocks[ out_video ]:
+        print( "    " + in_video )
+
+        for sub_video in video_blocks[ out_video ][ in_video ]:
+            timerange = video_blocks[ out_video ][ in_video ][ sub_video ][0] + " - " + video_blocks[ out_video ][ in_video ][ sub_video ][1]
+            print( "        " + timerange )
+    print( "" )
+
+for video in video_blocks:
 
     index = 0
+    for sub_video in video_blocks[ video ]:
+        for sub_clip in video_blocks[ video ][ sub_video ]:
 
-    video_name = video.rsplit( ".", 1 )[0]
-    temp_path = root_dir + "TEMP_SPLIT" + os.sep
-    out_dir = root_dir + "concat" + os.sep
-    CreateDirectory( temp_path )
-    CreateDirectory( out_dir )
+            start = SplitTime( video_blocks[ video ][ sub_video ][ sub_clip ][0] )
+            end = SplitTime( video_blocks[ video ][ sub_video ][ sub_clip ][1] )
 
-    for sub_clip in ffmpeg_split_dict[ video ]:
+            #dt_start = datetime.timedelta( hours = start[ "hr" ], minutes = start[ "min" ], seconds = start[ "sec" ], milliseconds = start[ "ms" ] )
+            dt_start = datetime.timedelta( hours = start[ "hr" ], minutes = start[ "min" ], seconds = start[ "sec" ] )
+            #dt_end = datetime.timedelta( hours = end[ "hr" ], minutes = end[ "min" ], seconds = end[ "sec" ], milliseconds = end[ "ms" ] )
+            dt_end = datetime.timedelta( hours = end[ "hr" ], minutes = end[ "min" ], seconds = end[ "sec" ] )
 
-        start = SplitTime( ffmpeg_split_dict[ video ][ sub_clip ][0] )
-        end = SplitTime( ffmpeg_split_dict[ video ][ sub_clip ][1] )
+            time_difference = dt_end.total_seconds() - dt_start.total_seconds()
+            dt_diff = datetime.timedelta( seconds = time_difference )
 
-        #dt_start = datetime.timedelta( hours = start[ "hr" ], minutes = start[ "min" ], seconds = start[ "sec" ], milliseconds = start[ "ms" ] )
-        dt_start = datetime.timedelta( hours = start[ "hr" ], minutes = start[ "min" ], seconds = start[ "sec" ] )
-        #dt_end = datetime.timedelta( hours = end[ "hr" ], minutes = end[ "min" ], seconds = end[ "sec" ], milliseconds = end[ "ms" ] )
-        dt_end = datetime.timedelta( hours = end[ "hr" ], minutes = end[ "min" ], seconds = end[ "sec" ] )
+            if os.sep in sub_video:
+                sub_video_split = sub_video.rsplit( os.sep, 1 )
 
-        time_difference = dt_end.total_seconds() - dt_start.total_seconds()
+                full_path = sub_video_split[0] + os.sep
+                sub_video_name = sub_video_split[1]
 
-        dt_diff = datetime.timedelta( seconds = time_difference )
+                if ":\\" not in full_path and os.name == "nt":
+                    full_path = root_dir + full_path
+            else:
+                full_path = root_dir
+                sub_video_name = sub_video
         
-        # we need to make sure this outputs to an mkv file
-        #main_audio = root_dir + "quad_audio" + os.sep + video_name + ".mka"
-        if video.endswith( ".mkv" ):
-            RunFFMpegSubVideo( root_dir + input_file_folder + video, True, temp_path + "sub_video_" + str( index ) + ".mkv", str( dt_start ), str( dt_diff ) )
-        else:
-            RunFFMpegSubVideo( root_dir + input_file_folder + video, False, temp_path + "sub_video_" + str( index ) + ".mkv", str( dt_start ), str( dt_diff ) )
+            RunFFMpegSubVideo( full_path + sub_video_name, temp_path + "sub_video_" + str( index ) + ".mkv", str( dt_start ), str( dt_diff ) )
 
-        index += 1
+            index += 1
 
     # ok, now combine the sub videos together
     sub_video_list = []
-    while( len( sub_video_list ) < index ):
-                                                                         # lol
+    while( len( sub_video_list ) < index ):                           # lol
         sub_video_list.append( temp_path + "sub_video_" + str( len( sub_video_list ) ) + ".mkv" )
 
-    RunFFMpegConCat( sub_video_list, out_dir + video_name + ".mkv" )
+    if os.sep in video:
+        out_video_split = video.rsplit( os.sep, 1 )
 
-RemoveDirectory( temp_path )
+        full_out_dir = out_video_split[0] + os.sep
+        out_video_name = out_video_split[1].rsplit( ".", 1 )[0]
+
+        if ":\\" not in full_out_dir and os.name == "nt":
+            full_out_dir = out_dir + full_out_dir
+    else:
+        full_out_dir = out_dir
+        out_video_name = video.rsplit( ".", 1 )[0]
+
+    CreateDirectory( full_out_dir )
+
+    RunFFMpegConCat( sub_video_list, full_out_dir + out_video_name + ".mkv" )
+
+# really not needed, and gives an OSError on linux saying it's not empty
+#RemoveDirectory( temp_path )
 
 print( "\n---------------------------------------------------------------" )
-print( "ok done" )
+print( "Finished" )
 print( "---------------------------------------------------------------\n" )
 
